bd83e5df748fed3373aa419253954f660cc51863
Ayush Anand <ayushanand@Ayushs-MacBook-Air.local>
2025-04-25 23:19:19 +0530

    modified

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typeData = new Typed(".role", {
+    strings: [
+      "Software Developer",
+      "Web Developer",
+      "Coder!",
+      "AI Enthusiast",
+      "AR Creator",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+});
